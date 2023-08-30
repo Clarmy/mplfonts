eb75f54cdf8a86240704474e200a169f7c583aaf
Bit0r <nie_wang@outlook.com>
2023-08-30 21:23:40 +0800

    修复Linux平台下init命令无法修改配置文件的问题

--- mplfonts/conf.py
+++ mplfonts/conf.py
@@ -1,6 +1,12 @@
 import os
+import platform
+from pathlib import Path
 
 BASE_DIR = os.path.dirname(__file__)
 
 FONT_DIR = os.path.join(BASE_DIR, "fonts")
-RC_DIR = os.path.join(BASE_DIR, "rc")
+if platform.system() == "Linux":
+    # Linux平台下，matplotlibrc文件的默认路径为~/.config/matplotlib/matplotlibrc
+    RC_DIR = str(Path('~/.config/matplotlib').expanduser().resolve())
+else:
+    RC_DIR = os.path.join(BASE_DIR, "rc")
