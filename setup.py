d6b332828ca1a871d05ee05c7adb805b60005ade
Clarmy Lee <clarmylee92510@gmail.com>
2024-12-02 03:52:30 +0800

    refactor: only def version once

--- setup.py
+++ setup.py
@@ -1,5 +1,22 @@
 import setuptools
 import os
+import codecs
+
+
+def read(rel_path):
+    here = os.path.abspath(os.path.dirname(__file__))
+    with codecs.open(os.path.join(here, rel_path), "r", encoding="utf-8") as fp:
+        return fp.read()
+
+
+def get_version(rel_path):
+    for line in read(rel_path).splitlines():
+        if line.startswith("__version__"):
+            delim = '"' if '"' in line else "'"
+            return line.split(delim)[1]
+    else:
+        raise RuntimeError("Unable to find version string.")
+
 
 FILE_PATH = os.path.dirname(os.path.realpath(__file__))
 
@@ -12,7 +29,7 @@ with open(requirements_path, encoding="utf-8") as f:
 
 setuptools.setup(
     name="mplfonts",
-    version="0.0.9",
+    version=get_version("mplfonts/__init__.py"),
     author="Wentao Li",
     author_email="clarmylee92510@gmail.com",
     description="Fonts manager for matplotlib",
